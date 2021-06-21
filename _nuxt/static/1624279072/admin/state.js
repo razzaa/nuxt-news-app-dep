09858fe03f1861ee27131386a8e5776873de3a7e
codewithali <ali-raza@ashlarglobal.com>
2021-06-21 17:38:27 +0500

    first commit

---
+++ _nuxt/static/1624279072/admin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-news-app-dep\u002F_nuxt\u002Fstatic\u002F1624279072",layout:"admin",error:a,state:a,serverRendered:false,routePath:"\u002Fadmin",config:{_app:{basePath:"\u002Fnuxt-news-app-dep\u002F",assetsPath:"\u002Fnuxt-news-app-dep\u002F_nuxt\u002F",cdnURL:a}}}}(null));
